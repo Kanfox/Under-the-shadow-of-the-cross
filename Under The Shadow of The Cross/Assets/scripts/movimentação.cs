c5a8f534392bd9ed1a388c8f9b1541548222dd9d
mariocssn <mario.marques@alunos.sc.senac.br>
2025-07-10 13:50:49 -0300

    projeto mario

--- Under The Shadow of The Cross/Assets/scripts/movimentação.cs
+++ Under The Shadow of The Cross/Assets/scripts/movimentação.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 
 public class NewMonoBehaviourScript : MonoBehaviour
 {
+    private float movimentoHorizontal;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
